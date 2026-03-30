04153aaf3c5d168b3a4d0a8fd5fbaca843ebe75b
AlaNo778 <Alan.labml@gmail.com>
2026-03-30 20:59:02 +0700

    Add project files.

---
+++ CoopEducation/Controllers/Login/TokenController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoopEducation.Controllers.Login
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+    }
+}
