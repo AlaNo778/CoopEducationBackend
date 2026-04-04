ef7a79960dcff46064e45bfdeee760b8d52ebd67
AlaNo778 <Alan.labml@gmail.com>
2026-04-04 15:38:05 +0700

    feat : FormController.

---
+++ CoopEducation/Controllers/DocAndForm/FormController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoopEducation.Controllers.DocAndForm
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FormController : ControllerBase
+    {
+    }
+}
