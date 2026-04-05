658613496622c73e17ccb3e59f22c6c0f60b16c5
AlaNo778 <Alan.labml@gmail.com>
2026-04-05 17:29:02 +0700

    Feat:  Model DTO Info Data.

---
+++ CoopEducation/Models/DTO/AdviseeStudentsDTO.cs
@@ -0,0 +1,30 @@
+﻿namespace CoopEducation.Models.DTO
+{
+    public class AdviseeStudentsDTO
+    {
+        public string StudentCode { get; set; } = null!;
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string? Email { get; set; }
+        public string? Faculty { get; set; }
+        public decimal? Gpax { get; set; }
+        public short? TotalCredits { get; set; } 
+        public string? MajorName { get; set; }
+        public string? PhoneHome { get; set; }
+        public string? PhoneMobile { get; set; }
+        public string? Facebook { get; set; }
+        public string? LineId { get; set; }
+        public string MentorFirstName { get; set; } = null!;
+        public string MentorLastName { get; set; } = null!;
+        public string? Position { get; set; }
+        public string? Department { get; set; }
+        public string? Phone { get; set; }
+        public string? MentorEmail { get; set; }
+        public string CompanyName { get; set; } = null!;
+        public string? JobTitle { get; set; }
+        public string? JobDescription { get; set; }
+        public DateOnly StartDate { get; set; }
+        public DateOnly EndDate { get; set; }
+        public string? AcademicYear { get; set; }
+    }
+}
---
+++ CoopEducation/Models/DTO/StudentInfoDTO.cs
@@ -0,0 +1,27 @@
+﻿namespace CoopEducation.Models.DTO
+{
+    public class StudentInfoDTO
+    {
+        public int StudentId { get; set; }
+        public string StudentCode { get; set; } = null!;
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string? Email { get; set; }
+        public string? Faculty { get; set; }
+        public decimal? Gpax { get; set; }
+        public short? TotalCredits { get; set; }
+        public string? MajorName { get; set; }
+        public string? HouseNo { get; set; }
+        public string? Road { get; set; }
+        public string? Alley { get; set; }
+        public string? VillageNo { get; set; }
+        public string? SubDistrict { get; set; }
+        public string? District { get; set; }
+        public string? Province { get; set; }
+        public string? Postcode { get; set; }
+        public string? PhoneHome { get; set; }
+        public string? PhoneMobile { get; set; }
+        public string? Facebook { get; set; }
+        public string? LineId { get; set; }
+    }
+}
