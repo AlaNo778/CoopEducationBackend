98f2c3118abc53d04130131c2cd227e208fbf8f3
AlaNo778 <Alan.labml@gmail.com>
2026-04-07 16:31:56 +0700

    Feat: UserId Model.

---
+++ CoopEducation/Models/DTO/UserInfoDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace CoopEducation.Models.DTO
+{
+    public class UserInfoDTO
+    {
+        public string FullName { get; set; } = null!;
+        public string RoleName { get; set; } = null!;
+    }
+}
